838b6799fd5c19a58f9eb301f69787a59cb40a9f
Bashir Alsuty <beshasuty125@hotmail.com>
2016-12-19 01:16:58 +0300

    Add files via upload

--- my_projects/wizard/project_doration.py
+++ my_projects/wizard/project_doration.py
@@ -1,1 +1,33 @@
+from openerp import models, fields, api
+import datetime
+
+class doration(models.TransientModel):
+	_name = 'project.doration'
+
+	doration_type = fields.Selection(string='Type', 
+									selection=[('years', 'Years'), 
+									('months', 'Months'), 
+									('days', 'Days')])
+
+
+	@api.multi
+	def calc_doration(self):
+		#get ids of records to be updated, then calcuate date doration
+		projects_ids = self.env['my.project'].browse(self._context.get('active_ids'))
+		for project in projects_ids:
+			start_date = project.start_date
+			end_date = project.end_date
+			if self.doration_type == 'years':
+				total_years_end = datetime.datetime.strptime(end_date, "%Y-%m-%d").year
+				total_years_start = datetime.datetime.strptime(start_date, "%Y-%m-%d").year
+				project.total =  str(total_years_end - total_years_start) + " yeas"
+			elif self.doration_type == 'months':
+				total_months_end = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+				total_months_start = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+				project.total = 'calc months here'
+			else: 
+				total_days_end = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+				total_days_start = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+				project.total = str((total_days_end - total_days_start).days) + " days" 
+		return True
 
