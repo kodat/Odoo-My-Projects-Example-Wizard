d0b060cd4f5e6ce957d9f144a4973241d1326bea
Bashir Alsuty <beshasuty125@hotmail.com>
2017-06-11 02:46:09 -0700

    Create projects_model.py

--- my_projects/projects_model.py
+++ my_projects/projects_model.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields
 
-
 STATE = [('draft', 'Draft'), 
 		('in_progress', 'In Progress'),
 		 ('cancel', 'Cancel'), 
 		 ('done', 'Done')]
 class project(models.Model):
+	#model name
 	_name = 'my.project'
-
+	
 	name = fields.Char('Name', required=True)
+	
+	
 	description = fields.Text('Description')
 	start_date = fields.Date('Start Date')
 	end_date = fields.Date('End Date')
